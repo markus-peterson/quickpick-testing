ceb4bccdb44908926032215f263b33680cf69065
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-06 00:09:42 -0500

    Update User.java

--- src/main/java/com/backend/model/User.java
+++ src/main/java/com/backend/model/User.java
@@ -35,56 +35,6 @@ public class User {
 	private String address;
 	private String password;
 	
-	public String profileFileId;
-	public String resumeFileId;
-	
-	public String getProfileFileId() {
-		return profileFileId;
-	}
-	public void setProfileFileId(String profileFileId) {
-		this.profileFileId = profileFileId;
-	}
-	public String getResumeFileId() {
-		return resumeFileId;
-	}
-	public void setResumeFileId(String resumeFileId) {
-		this.resumeFileId = resumeFileId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+	private String profileFileId;
+	private String resumeFileId;
 }
