f3444910caec98aa7711037017a35109296924a6
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-15 15:28:15 -0500

    Add files via upload

--- src/main/java/com/quickpick/BackendQuickPickApplication.java
+++ src/main/java/com/quickpick/BackendQuickPickApplication.java
@@ -1,10 +1,19 @@
 package com.quickpick;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 @ComponentScan({"com.backend.controller","com.backend.service","com.backend.Springsecurity.Configuration"})
@@ -16,4 +25,17 @@ public class BackendQuickPickApplication {
 		SpringApplication.run(BackendQuickPickApplication.class, args);
 	}
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
 }
