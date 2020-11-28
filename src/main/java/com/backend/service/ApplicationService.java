1628b496341c28c657f572c7b0751e7223b4c284
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-28 03:16:53 -0500

    Add files via upload

--- src/main/java/com/backend/service/ApplicationService.java
+++ src/main/java/com/backend/service/ApplicationService.java
@@ -1,12 +1,15 @@
 package com.backend.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.backend.dao.ApplicationDao;
+import com.backend.dao.UserDao;
 import com.backend.model.Application;
+import com.backend.model.User;
 
 @Service
 public class ApplicationService {
@@ -14,14 +17,30 @@ public class ApplicationService {
 	@Autowired
 	private ApplicationDao appDao;
 	
+	@Autowired
+	private UserDao userDao;
+	
 	public Application addApplication(Application app) {
 		Application newApp = app;
 		newApp.setStatus("Pending");
 		return appDao.save(newApp);
 	}
 	
-	public List<Application> getApplicants(String jobId) {
-		return appDao.findAllByJobId(jobId);
+	public List<Object> getApplicants(String jobId) {
+		List<Application> apps = appDao.findAllByJobId(jobId);
+		List<User> users = new ArrayList<>();
+		List<Object> out = new ArrayList<>();
+		
+		for(Application app : apps) {
+			User user = userDao.findById(app.getUserId()).orElse(null);
+			if(user != null) {
+				user.setPassword("");
+				users.add(user);
+			}
+		}
+		out.add(apps);
+		out.add(users);
+		return out;
 	}
 	
 	public List<Application> getUserApps(String userId){
@@ -31,7 +50,7 @@ public class ApplicationService {
 	public Application acceptApplicant(String jobId, String userId) {
 		List<Application> appList = appDao.findAllByJobId(jobId);
 		for(Application app : appList) {
-			if(app.getUserId().equals(userId) && app.getStatus().equals("Pending")) {
+			if(app.getUserId().equals(userId)) {
 				app.setStatus("Accepted");
 				return appDao.save(app);
 			}
